c1b519adbe0f38d3e06b468d88f57dc7bfeb57a6
Anita Bahmanyar <anita@user3-26-17.wireless.utoronto.ca>
2014-11-03 18:11:08 -0500

    Q1 animation works

--- lab9/lab9_q1.py
+++ lab9/lab9_q1.py
@@ -6,7 +6,7 @@ Student number: 998909098
 import numpy as np
 import matplotlib.pylab as plt
 import cmath as cm
-import banded
+from   banded import banded
 
 ##########################################
                #constants#
@@ -27,32 +27,34 @@ hbar  = h_planck/(2*np.pi)
 #This has the shape of a gaussian function
 ###########################################
 def si_initial(x):
-    value = np.exp(-((x-x0)**2)/(2*(sigma**2))) * cm.exp(kappa*x)
+    value = np.exp(-((x-x0)**2)/(2*(sigma**2))) * np.exp(1j*kappa*x)
     return value
-
+'''
 #returns si for an array of x values
 def si_return(x):
     si  =np.zeros(len(x))
     for i in range(len(x)):
         si[i] = si_initial(x[i])
     return si
+'''
 
 a1 = 1 + (h*(complex(hbar)/(2*M*(a**2))))
 a2 = -h*(complex(hbar)/(4*M*(a**2)))
 #########################################################
 #creating matrix A where n is the dimension of the matrix
 #########################################################
-def A(n):
-    Avals = np.zeros([n,n])
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                Avals[i][j] = a1.real
-            elif abs(i-j) == 1:
-                Avals[i][j] = a2.real
-            else:
-                Avals[i][j] = 0
-    return Avals
+def A(n,a1,a2):
+    a_diag = np.zeros(n,complex)
+    a_sup  = np.zeros(n,complex)
+    a_sub  = np.zeros(n,complex)
+
+    a_diag[0]     = 1
+    a_diag[n-1]   = 1
+    a_diag[1:n-1] = a1
+    a_sup[2:]     = a2
+    a_sub[:-2]    = a2
+    
+    return np.array([a_sup,a_diag,a_sub])
 
 
 b1 = 1 - (h*(complex(hbar)/(2*M*(a**2))))
@@ -60,31 +62,39 @@ b2 = h*(complex(hbar)/(4*M*(a**2)))
 
 #defining v matrix as in page 441
 def v(si):
-    matrix = np.zeros(len(si))
+    matrix = np.zeros(len(si),complex)
     for i in range(len(si)-1):
-        matrix[i] = (b1.real*si[i]) + b2.real*(si[i+1]+si[i-1])
+        matrix[i] = (b1*si[i]) + b2*(si[i+1]+si[i-1])
     return matrix
 
-up   = 3
-down = 3
-
+up   = 1
+down = 1
 xvalues = np.arange(0,L,a)
 
+si_vals = si_initial(xvalues)
 ###plotting part for animation
-#plt.ion()
-#fig  = plt.figure()
-#ax   = plt.axes(xlim=(0,1),ylim=(-0.006,0.008))
-#line = ax.plot(x,si,'-b')
-#plt.show()
+plt.ion()
+fig  = plt.figure()
+ax   = plt.axes(xlim=(0,1e-8))
+line = ax.plot(xvalues,si_vals,'-b')
+plt.show()
 
-#solving for Ax = v
-si_new = banded(A(len(xvalues)),v(si_return(xvalues)),up,down)
 
-#making animation
-#line[0].set_ydata(si_new)
-#plt.draw()
-#t +=deltat
+#calling A function to create the matrix
+A       = A(len(xvalues),a1,a2)
 
+t    = 0
+tend = 1e-15 #seconds
+si_solutions = [] #an empty list to include si values
+while t<tend:
+    si_new = banded(A,v(si_vals),up,down)
+    si_solutions.append(si_new)
+    si_vals = si_new
+    
+    line[0].set_ydata(si_new)
+    plt.draw()
+    print t
+    t += h
 
 
 
