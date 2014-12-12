ca01331130dbdc73b9182921b98b13430539ec50
Anita Bahmanyar <anita@user66-106.vicres.utoronto.ca>
2014-12-11 22:19:33 -0500

    matri a works sort of

--- independent/cubic_spline.py
+++ independent/cubic_spline.py
@@ -1,60 +1,135 @@
 import numpy as np
 import matplotlib.pylab as plt
 
+#----------------------------------------------------------------
+#                         Functions
+#----------------------------------------------------------------
 
-#define a function to calculate start point
+#function to calculate start point
 def start_point(x0,y0,x1,y1):
     '''
     generating equation for k0
     '''
-    val = (2./x1-x0)*k0 + (1./x1-x0)*k1 - (3.*(y1-y0)/((x1-x0)**2))
-    return val
+    a11 = (2./(x1-x0))              #a matrix
+    a12 = (1./(x1-x0))              #a matrix
+    b   = (3.*(y1-y0)/((x1-x0)**2)) #b matrix
+    return a11,a12,b
 
-#define a function to calculate end point
-def end_point(xn-1,yn-1,xn,yn):
+#function to calculate end point
+def end_point(xn_1,yn_1,xn,yn):
     '''
     generating equation for kn
     '''
-    val = (1./xn-xn-1)*kn-1 + (2./xn-xn-1)*kn - (3.*(yn-yn-1)/((xn-xn-1)**2))
-    return val
+    a1n_1 = (1./(xn-xn_1))                #a matrix
+    a1n   = (2./(xn-xn_1))                #a matrix
+    b     = (3.*(yn-yn_1)/((xn-xn_1)**2)) #b matrix
+    return a1n_1,a1n,b
 
 
-#define a function to calculate middle points
-def mid_points(xi-1,yi-1,xi,yi,xi+1,yi+1):
+#function to calculate middle points
+def mid_points(xi_1,yi_1,xi,yi,xip1,yip1):
     '''
     generating equations for k1-kn-1
     '''
-    vals = (ki-1/xi-xi-1) + ((1./xi-xi-1)+(1./xi+1-xi))*2*ki + (ki+1/(xi+1-xi)) - (3.*((yi-yi-1/((xi-xi-1)**2)) + (yi+1-yi/((xi+1-xi)**2))))
-    return vals
+    aki_1 = (1./(xi-xi_1))                    #a matrix
+    aki   = ((1./(xi-xi_1))+(1./(xip1-xi)))*2 #a matrix
+    akip1 = (1./(xip1-xi))                    #a matrix
+    return aki_1,aki,akip1
 
-#define a function to solve the n+1 equations
-def solver(a,k):
+#function to return b values for mid points
+def b_vals(xi_1,yi_1,xi,yi,xip1,yip1):
+    '''
+    generating equations for k1-kn-1
+    '''
+    b =   3.*(((yi-yi_1)/((xi-xi_1)**2)) + ((yip1-yi)/((xip1-xi)**2)))
+    return b
+
+
+#function to generate the a matrix, n is number of points
+def gen_matrix(n,a11,a12,a1n_1,a1n,points):
+    a = np.zeros([n,n])
+    a[0][0]   = a11
+    a[0][1]   = a12
+    a[0][2:]  = 0.
+    
+    xi_1 = points[0][0]
+    yi_1 = points[0][1]
+    xi   = points[1][0]
+    yi   = points[1][1]
+    xip1 = points[2][0]
+    yip1 = points[2][1]
+    
+    a[1][:]   = mid_points(xi_1,yi_1,xi,yi,xip1,yip1)
+    a[2][:-2] = 0.0
+    a[2][-2]  = a1n_1
+    a[2][-1]  = a1n
+    return a
+
+
+#function to solve the n+1 equations
+def solver(a,b):
     '''
     solves n equations using numpy solver module
     '''
-    val = np.linalg.solve(a,k)
+    val = np.linalg.solve(a,b)
     return val
 
-#define a function to calculate a_i and b_i
-def ab():
+#function to calculate a_i and b_i
+def ab(xi_1,yi_1,xi,yi,ki_1,ki):
     '''
     returns a and b coefficients using k values from the matrix solving
     '''
-    ai = ki-1*(xi-xi-1) - (yi-yi-1)
-    bi = -ki*(xi-xi-1)  + (yi-yi-1)
+    ai = ki_1*(xi-xi_1) - (yi-yi_1)
+    bi = -ki*(xi-xi_1)  + (yi-yi_1)
     return ai,bi
 
-#define a function to calculate q_i
-def q(t):
-    qi = ((1-t)*yi-1) + t*yi + t*(1-t)*(ai*(1-t) + bi*t)
+#function to calculate q_i
+def q(yi_1,yi,ai,bi,t):
+    qi = ((1-t)*yi_1) + t*yi + t*(1-t)*(ai*(1-t) + bi*t)
     return qi
 
-#define a function to find t
-def t(x1,x2):
-    tval = x-x1/x2-x1
+#function to find t
+def t(x,x1,x2):
+    tval = (x-x1)/(x2-x1)
     return tval
 
-#test it with normal sine function and also step function
-x = np.linspace(0.0,10.0,5)
-y = np.sin(x)
+#----------------------------------------------------------------
+#                        Code Starts Here
+#----------------------------------------------------------------
+
+#number of points
+n      = 4
+points = np.array([[-1.,0.5],[0.0,0.0],[3.0,3.0],[1.0,2.0]])
+a      = np.zeros([n,n])
+b      = np.zeros(n)
+
+
+#generating first and last row of the a matrix
+a11,a12,b1    = start_point(points[0][0],
+                            points[0][1],points[1][0],points[1][1])
+a1n_1, a1n,bn = end_point(points[n-2][0],
+                          points[n-2][1],points[n-1][0],points[n-1][1])
+
+a[0][0]     = a11
+a[0][1]     = a12
+a[n-1][-2]  = a1n_1
+a[n-1][-1]  = a1n
+
+b[0]   = b1
+b[n-1] = bn
+
+#generating a and b matrices
+for i in range(1,n-1,1):
+    print i
+    print points[i-1][0],points[i-1][1],points[i][0],points[i][1],points[-+1][0],points[i+1][1]
+    a[i][:]   = mid_points(points[i-1][0],points[i-1][1],points[i][0],points[i][1],points[-+1][0],points[i+1][1])
+    b[i] = b_vals(points[i-1][0],points[i-1][1],points[i][0],points[i][1],points[-+1][0],       points[i+1][1])
+
+#solving for k values
+k = solver(a,b)
+
+#getting ai and bi values
+
+
+
 
